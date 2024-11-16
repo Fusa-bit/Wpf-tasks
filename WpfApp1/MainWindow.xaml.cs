6aa1b9f6dccabfeb1716675ae47cff0283187ec2
Лунная Аквилегия <bettyglichtale3@gmail.com>
2024-11-16 14:44:16 +1000

    add calc ui те файлы проекта.

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Win32;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,9 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        //private string imgsource1 = @"\\sysprofiles.adm.vvsu.ru\STUDENTRPROFILES$\Anna_Petrenko\Downloads\d.jpg";
+        //private string imgsource2 = @"\\sysprofiles.adm.vvsu.ru\STUDENTRPROFILES$\Anna_Petrenko\Downloads\d.jpg";
         public MainWindow()
         {
             InitializeComponent();
@@ -31,5 +35,31 @@ namespace WpfApp1
             Window1.Show();
             this.Close();
         }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Приложение закроется");
+            Application.Current.Shutdown();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            TextBlock.Text = "SUS";
+            //DisplayImage.Source = new BitmapImage(new Uri(imgsource1));
+            //OpenFileDialog openFileDialog = new OpenFileDialog { Filter ="Image Files| *.jpg;*.jpeg;*.png;*.gif;*.bmp;"};
+            //if (openFileDialog.ShowDialog() == true)
+            //{
+            //BitmapImage bitmapImage = new BitmapImage(new Uri(openFileDialog.FileName));
+            //DisplayImage.Source = bitmapImage;
+
+            //}
+        }
+
+        //private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        //{
+            //DisplayImage.Source = new BitmapImage(new Uri(imgsource2));
+        //}
+        //<MenuItem Header="Файл1" Click="MenuItem_Click_1"></MenuItem>
+        //<MenuItem Header = "Файл2" Click="MenuItem_Click_2"></MenuItem>
     }
 }
